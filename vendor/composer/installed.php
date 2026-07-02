fccb7ea0492f982c5a4b17d62e7f7ebc4c5f0b65
Gusryandra <gusryan5246@gmail.com>
2026-07-02 10:18:58 +0000

    Index html sudah

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b8805ee2f7dad301d960f5008545f3d93b009e66',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b8805ee2f7dad301d960f5008545f3d93b009e66',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
